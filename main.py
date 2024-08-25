9e767c2ffe7772491544eda705b437cf6485a3d3
RdotSilva <RyandotSilva@gmail.com>
2024-08-25 19:51:42 +0100

    Add prompt logic

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello Agent")
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from dotenv import load_dotenv
+
+from tools.sql import run_query_tool
+
+load_dotenv()
+
+chat = ChatOpenAI()
+prompt = ChatPromptTemplate(
+    messages=[
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
